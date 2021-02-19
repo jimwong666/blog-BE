abc04495ae6ddadbab2ae4ae57e9f937873917f4
锤锤 <798165604@qq.com>
2021-02-19 14:53:30 +0800

    edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { LoginModule } from './server/login/login.module';
 import { ApiModule } from './server/api/api.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
